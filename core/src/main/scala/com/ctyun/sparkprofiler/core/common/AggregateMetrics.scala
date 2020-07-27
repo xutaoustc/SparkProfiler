79e7218218483c2c6957beb9880b86c47961283a
xutao <xutao_ustc@163.com>
2020-07-27 11:19:03 +0800

    整理

--- core/src/main/scala/com/ctyun/sparkprofiler/core/common/AggregateMetrics.scala
+++ core/src/main/scala/com/ctyun/sparkprofiler/core/common/AggregateMetrics.scala
@@ -1,126 +1,13 @@
 package com.ctyun.sparkprofiler.core.common
 
-import java.util.Locale
-
 import org.apache.spark.executor.TaskMetrics
 import org.apache.spark.scheduler.TaskInfo
-import org.json4s.DefaultFormats
-import org.json4s.JsonAST.JValue
-
 import scala.collection.mutable
-
 
 class AggregateMetrics() {
   var count = 0L
   val map = new mutable.LinkedHashMap[AggregateMetrics.Metric, AggregateValue]()
 
-  @transient val formatterMap = new mutable.HashMap[AggregateMetrics.Metric,
-                                                    ((AggregateMetrics.Metric, AggregateValue), mutable.StringBuilder) => Unit
-                                                   ]()
-
-  formatterMap(AggregateMetrics.taskDuration)= formatMillisTime
-  formatterMap(AggregateMetrics.executorRuntime) = formatMillisTime
-  formatterMap(AggregateMetrics.executorCpuTime)= formatNanoTime
-  formatterMap(AggregateMetrics.jvmGCTime) = formatMillisTime
-
-  formatterMap(AggregateMetrics.resultSize)= formatBytes
-  formatterMap(AggregateMetrics.inputBytesRead)= formatBytes
-  formatterMap(AggregateMetrics.outputBytesWritten)= formatBytes
-  formatterMap(AggregateMetrics.memoryBytesSpilled)= formatBytes
-  formatterMap(AggregateMetrics.diskBytesSpilled)= formatBytes
-  formatterMap(AggregateMetrics.peakExecutionMemory)= formatBytes
-
-  formatterMap(AggregateMetrics.shuffleWriteTime) = formatNanoTime
-  formatterMap(AggregateMetrics.shuffleWriteBytesWritten) = formatBytes
-  formatterMap(AggregateMetrics.shuffleWriteRecordsWritten) = formatRecords
-  formatterMap(AggregateMetrics.shuffleReadFetchWaitTime) = formatNanoTime
-  formatterMap(AggregateMetrics.shuffleReadBytesRead) = formatBytes
-  formatterMap(AggregateMetrics.shuffleReadRecordsRead) = formatRecords
-  formatterMap(AggregateMetrics.shuffleReadLocalBlocks)= formatRecords
-  formatterMap(AggregateMetrics.shuffleReadRemoteBlocks) = formatRecords
-
-
-
-
-
-  @transient val numberFormatter = java.text.NumberFormat.getIntegerInstance
-
-  def bytesToString(size: Long): String = {
-    val TB = 1L << 40
-    val GB = 1L << 30
-    val MB = 1L << 20
-    val KB = 1L << 10
-
-    val (value, unit) = {
-      if (Math.abs(size) >= 1*TB) {
-        (size.asInstanceOf[Double] / TB, "TB")
-      } else if (Math.abs(size) >= 1*GB) {
-        (size.asInstanceOf[Double] / GB, "GB")
-      } else if (Math.abs(size) >= 1*MB) {
-        (size.asInstanceOf[Double] / MB, "MB")
-      } else {
-        (size.asInstanceOf[Double] / KB, "KB")
-      }
-    }
-    "%.1f %s".formatLocal(Locale.US, value, unit)
-  }
-
-  def toMillis(size:Long): String = {
-    val MS  = 1000000L
-    val SEC = 1000 * MS
-    val MT  = 60 * SEC
-    val HR  = 60 * MT
-
-    val (value, unit) = {
-      if (size >= 1*HR) {
-        (size.asInstanceOf[Double] / HR, "hh")
-      } else if (size >= 1*MT) {
-        (size.asInstanceOf[Double] / MT, "mm")
-      } else if (size >= 1*SEC) {
-        (size.asInstanceOf[Double] / SEC, "ss")
-      } else {
-        (size.asInstanceOf[Double] / MS, "ms")
-      }
-    }
-    "%.1f %s".formatLocal(Locale.US, value, unit)
-  }
-
-  def formatNanoTime(x: (AggregateMetrics.Metric, AggregateValue), sb: mutable.StringBuilder): Unit = {
-    sb.append(f" ${x._1}%-30s${toMillis(x._2.value)}%20s${toMillis(x._2.min)}%15s${toMillis(x._2.max)}%15s${toMillis(x._2.mean.toLong)}%20s")
-      .append("\n")
-  }
-
-  def formatMillisTime(x: (AggregateMetrics.Metric, AggregateValue), sb: mutable.StringBuilder): Unit = {
-    def addUnits(x: Long): String = {
-      toMillis(x * 1000000)
-    }
-    sb.append(f" ${x._1}%-30s${addUnits(x._2.value)}%20s${addUnits(x._2.min)}%15s${addUnits(x._2.max)}%15s${addUnits(x._2.mean.toLong)}%20s")
-      .append("\n")
-  }
-
-  def formatBytes(x: (AggregateMetrics.Metric, AggregateValue), sb: mutable.StringBuilder): Unit = {
-    sb.append(f" ${x._1}%-30s${bytesToString(x._2.value)}%20s${bytesToString(x._2.min)}%15s${bytesToString(x._2.max)}%15s${bytesToString(x._2.mean.toLong)}%20s")
-      .append("\n")
-  }
-
-  def formatRecords(x: (AggregateMetrics.Metric, AggregateValue), sb: mutable.StringBuilder): Unit = {
-    sb.append(f" ${x._1}%-30s${numberFormatter.format(x._2.value)}%20s${numberFormatter.format(x._2.min)}%15s${numberFormatter.format(x._2.max)}%15s${numberFormatter.format(x._2.mean.toLong)}%20s")
-      .append("\n")
-  }
-
-  def updateMetric(metric: AggregateMetrics.Metric, newValue: Long) : Unit = {
-    val aggregateValue = map.getOrElse(metric, new AggregateValue)
-    if (count == 0) {
-      map(metric) = aggregateValue
-    }
-    aggregateValue.value +=  newValue
-    aggregateValue.max    = math.max(aggregateValue.max, newValue)
-    aggregateValue.min    = math.min(aggregateValue.min, newValue)
-    val delta: Double     = newValue - aggregateValue.mean
-    aggregateValue.mean  += delta/(count+1)
-    aggregateValue.m2 += delta * (newValue - aggregateValue.mean)
-    aggregateValue.variance = aggregateValue.m2 / (count+1)
-  }
 
   def updateAggregateTaskMetrics(tm: TaskMetrics, ti: TaskInfo): Unit = {
     updateMetric(AggregateMetrics.taskDuration,             ti.duration)
@@ -146,18 +33,15 @@ class AggregateMetrics() {
     count += 1
   }
 
-  def print(caption: String, sb: mutable.StringBuilder):Unit = {
-    sb.append(s" AggregateMetrics (${caption}) total measurements ${count} ")
-      .append("\n")
-    sb.append(f"                NAME                        SUM                MIN           MAX                MEAN         ")
-      .append("\n")
-    map.toBuffer.foreach[Unit](x => {
-      formatterMap(x._1)(x, sb)
-    })
-  }
-
-  def getMap(): Map[String, Any] = {
-    Map("count" -> count, "map" -> map.keys.map(key => (key.toString, map.get(key).get.getMap())).toMap)
+  private def updateMetric(metric: AggregateMetrics.Metric, newValue: Long) : Unit = {
+    val aggregateValue = map.getOrElseUpdate(metric, new AggregateValue)
+    aggregateValue.value +=  newValue
+    aggregateValue.max    = math.max(aggregateValue.max, newValue)
+    aggregateValue.min    = math.min(aggregateValue.min, newValue)
+    val delta: Double     = newValue - aggregateValue.mean
+    aggregateValue.mean  += delta/(count+1)
+    aggregateValue.m2    += delta * (newValue - aggregateValue.mean)
+    aggregateValue.variance = aggregateValue.m2 / (count+1)
   }
 }
 
@@ -183,5 +67,4 @@ object AggregateMetrics extends Enumeration {
   peakExecutionMemory,
   taskDuration
   = Value
-
 }
--- core/src/main/scala/com/ctyun/sparkprofiler/core/common/AggregateValue.scala
+++ core/src/main/scala/com/ctyun/sparkprofiler/core/common/AggregateValue.scala
@@ -1,8 +1,5 @@
 package com.ctyun.sparkprofiler.core.common
 
-import org.json4s.DefaultFormats
-import org.json4s.JsonAST.JValue
-
 class AggregateValue {
   var value:    Long   = 0L
   var min:      Long   = Long.MaxValue
@@ -29,20 +26,5 @@ class AggregateValue {
       "mean" -> mean,
       "m2" -> m2,
       "variance" -> variance)
-  }
-}
-
-object AggregateValue {
-  def getValue(json: JValue): AggregateValue = {
-    implicit val formats = DefaultFormats
-
-    val value = new AggregateValue
-    value.value = (json  \ "value").extract[Long]
-    value.min = (json \ "min").extract[Long]
-    value.max = (json \ "max").extract[Long]
-    value.mean = (json \ "mean").extract[Double]
-    value.variance = (json \ "variance").extract[Double]
-    value.m2 = (json \ "m2").extractOrElse[Double](0.0)
-    value
   }
 }
